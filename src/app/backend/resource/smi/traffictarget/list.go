5d0331d72aeb61b2fa9e9e0fd8102673d88a36b6
admin <admin@localhost.localdomain>
2022-06-24 06:15:07 -0400

    add traffictarget resource and api

---
+++ src/app/backend/resource/smi/traffictarget/list.go
@@ -0,0 +1,81 @@
+package traffictarget
+
+import (
+	"log"
+
+	smiaccessv1alpha3 "github.com/servicemeshinterface/smi-sdk-go/pkg/apis/access/v1alpha3"
+	smiaccessclientset "github.com/servicemeshinterface/smi-sdk-go/pkg/gen/client/access/clientset/versioned"
+
+	"github.com/kubernetes/dashboard/src/app/backend/api"
+	"github.com/kubernetes/dashboard/src/app/backend/errors"
+	"github.com/kubernetes/dashboard/src/app/backend/resource/common"
+	"github.com/kubernetes/dashboard/src/app/backend/resource/dataselect"
+)
+
+// TrafficTarget is a representation of a traffictarget.
+type TrafficTarget struct {
+	ObjectMeta api.ObjectMeta `json:"objectMeta"`
+	TypeMeta   api.TypeMeta   `json:"typeMeta"`
+}
+
+// TrafficTargetList contains a list of services in the cluster.
+type TrafficTargetList struct {
+	ListMeta api.ListMeta `json:"listMeta"`
+
+	// Unordered list of traffictargets.
+	TrafficTargets []TrafficTarget `json:"traffictargets"`
+
+	// List of non-critical errors, that occurred during resource retrieval.
+	Errors []error `json:"errors"`
+}
+
+// GetServiceList returns a list of all services in the cluster.
+func GetTrafficTargetList(smiAccessClient smiaccessclientset.Interface, nsQuery *common.NamespaceQuery,
+	dsQuery *dataselect.DataSelectQuery) (*TrafficTargetList, error) {
+	log.Print("Getting list of all services in the cluster")
+
+	channels := &common.ResourceChannels{
+		TrafficTargetList: common.GetTrafficTargetListChannel(smiAccessClient, nsQuery, 1),
+	}
+
+	return GetTrafficTargetListFromChannels(channels, dsQuery)
+}
+
+// GetTrafficTargetListFromChannels returns a list of all services in the cluster.
+func GetTrafficTargetListFromChannels(channels *common.ResourceChannels,
+	dsQuery *dataselect.DataSelectQuery) (*TrafficTargetList, error) {
+	trafficTargets := <-channels.TrafficTargetList.List
+	err := <-channels.TrafficTargetList.Error
+	nonCriticalErrors, criticalError := errors.HandleError(err)
+	if criticalError != nil {
+		return nil, criticalError
+	}
+
+	return CreateTrafficTargetList(trafficTargets.Items, nonCriticalErrors, dsQuery), nil
+}
+
+func toTrafficTarget(trafficTarget *smiaccessv1alpha3.TrafficTarget) TrafficTarget {
+	return TrafficTarget{
+		ObjectMeta:        api.NewObjectMeta(trafficTarget.ObjectMeta),
+		TypeMeta:          api.NewTypeMeta(api.ResourceKindService),
+	}
+}
+
+// CreateTrafficTargetList returns paginated traffictarget list based on given traffictarget array and pagination query.
+func CreateTrafficTargetList(trafficTargets []smiaccessv1alpha3.TrafficTarget, nonCriticalErrors []error, dsQuery *dataselect.DataSelectQuery) *TrafficTargetList {
+	trafficTargetsList := &TrafficTargetList{
+		TrafficTargets: make([]TrafficTarget, 0),
+		ListMeta: api.ListMeta{TotalItems: len(trafficTargets)},
+		Errors:   nonCriticalErrors,
+	}
+
+	trafficTargetCells, filteredTotal := dataselect.GenericDataSelectWithFilter(toCells(trafficTargets), dsQuery)
+	trafficTargets = fromCells(trafficTargetCells)
+	trafficTargetsList.ListMeta = api.ListMeta{TotalItems: filteredTotal}
+
+	for _, trafficTarget := range trafficTargets {
+		trafficTargetsList.TrafficTargets = append(trafficTargetsList.TrafficTargets, toTrafficTarget(&trafficTarget))
+	}
+
+	return trafficTargetsList
+}
